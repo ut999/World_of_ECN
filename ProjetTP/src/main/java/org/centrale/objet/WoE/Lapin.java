7bd6b14c178001d4821f4e82f963e5e94f5d1d17
achille Bayon <utaabn@gmail.com>
2025-09-19 17:01:47 +0200

    Create Lapin

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Lapin.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author utaab
+ */
+public class Lapin extends Monstre {
+
+    public Lapin(int ptVie, int degAtt, int ptPar, int pageAtt, int pagePar, Point2D pos) {
+        super(ptVie, degAtt, ptPar, pageAtt, pagePar, pos);
+    }
+
+    public Lapin(Lapin l) {
+        super(l);
+    }
+
+    public Lapin() {
+        super();
+    }
+}
