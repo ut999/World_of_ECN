c4b2d69f2a660df08be9f781ccaebd7b3982a112
achille Bayon <utaabn@gmail.com>
2025-09-19 16:19:20 +0200

    Creation du projet

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/TestWoE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author utaab
+ */
+public class TestWoE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
