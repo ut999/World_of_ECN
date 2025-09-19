9cfce5571d67a3f5f537105698ca7a96c473cef3
achille Bayon <utaabn@gmail.com>
2025-09-19 16:42:56 +0200

    Creation Point2D

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Point2D.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author utaab
+ */
+public class Point2D {
+    private int x;
+    private int y;
+
+    public Point2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point2D(Point2D p) {
+        this.x = p.x;
+        this.y = p.y;
+    }
+    
+    public Point2D() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    public void setPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void translate(int dx, int dy) {
+        this.x = x + dx;
+        this.y = y + dy;
+    }
+    
+    public void affiche(int dx, int dy) {
+        System.out.println("Position :\nx : " + x + "\ny : " + y);
+    }
+}
